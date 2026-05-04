8525dc2cb6d7590f1b3bd94fdfd81a054f9aa1d6
Sumayea Rahman <shorbonam104@gmail.com>
2026-05-04 22:46:53 +0600

    primitive data type

---
+++ jsDunia.js
@@ -0,0 +1,4 @@
+let userName = "mejba123";
+userName = 123 ;
+
+console.log("user Name:", userName);
---
+++ src/primitive.ts
@@ -0,0 +1,27 @@
+// js-ts
+//string , Number , boolean, undefined, null
+// Special-TS: never , unknown , void
+
+let userName: string = "majba123";
+
+userName = 123 
+
+
+let userID = 123
+
+// userName.
+
+
+userID.toFixed()
+userID.toFixed
+
+
+let isAdmin : boolean = false
+isAdmin = true
+
+let x: undefined = undefined;
+// let x: any = undefined;
+
+
+x = 5
+x = "five"
