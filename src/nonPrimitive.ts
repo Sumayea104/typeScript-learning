6b03fe6b7f7006a407d3eae1c70f480f272a18fd
Sumayea Rahman <shorbonam104@gmail.com>
2026-05-05 22:49:10 +0600

    non primitive - array objest toople

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,27 @@
+//array, object
+
+let bazarList: string= ['eggs','milk','sugar', 'banana'];
+
+bazarList.push(true);
+
+let mixedArr: (string | number)[] = ['eggs',12, 'milk', 1, 'sugar', 2]
+
+mixedArr.push(true);
+
+//ts toople
+
+let coordinates : [number,number]= [20, 30, 40];
+
+let couple : string= ['husband', 'wife'];
+
+let nameAndRoll = ['mezba', 79];
+
+let mezbaNameAndRoll : [ string, number] = ['mezba', 79];
+
+mezbaNameAndRoll[0] = 79
+
+mezbaNameAndRoll[0] = 'mezba'
+
+//toople can be 3 elements
+
+let destination : [string, string, number]= ['dhaka', 'chattogram', 3]
--- src/primitive.ts
+++ src/primitive.ts
@@ -2,25 +2,33 @@
 //string , Number , boolean, undefined, null
 // Special-TS: never , unknown , void
 
-//let userName: string = "majba123";
+let userName: string = "mezba123";
+
+//userName = mezba321
+
+userName = 'mezba321'
 
 //userName = 123 
 
 
 let userID = 123
 
-// userName.
+userID = '123'
+
+// userName. [methods suggestion dae, jotogula method available ta janae]
 
 
-userID.toFixed()
+userName.toFixed()
 userID.toFixed
 
+//let isAdmin = false  [data type bole na dileo ts bujhe nae eta holo - implicit]
 
-let isAdmin : boolean = false
+let isAdmin : boolean = false //[ directly bole dea hole ota - explici] 
+
 isAdmin = true
 
-let x: undefined = undefined;
-// let x: any = undefined;
+//let x: undefined = undefined;
+let x: any = undefined; // define na korle any dekhabe, so pore jekono kichu assign kora jae
 
 
 x = 5
