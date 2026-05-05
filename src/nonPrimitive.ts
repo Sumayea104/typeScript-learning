f4279d226fbca3f77d67b93afc6a4e944c767982
Sumayea Rahman <shorbonam104@gmail.com>
2026-05-05 23:27:21 +0600

    object type

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -25,3 +25,53 @@ mezbaNameAndRoll[0] = 'mezba'
 //toople can be 3 elements
 
 let destination : [string, string, number]= ['dhaka', 'chattogram', 3]
+
+// reference type : object
+
+// const user: {
+//     organization : "programming hero";//value k type hishabe use korteche - eta k bole literel type
+//     firstName : string;
+//     middleName? : string; // optional type
+//     lastName : string;
+//     isMarried : boolean;
+// } =
+// // { firstName : "mezba",
+// //     middleName : "abedin",
+// //     lastName : "forhan",
+// //     isMarried : true,
+// // };
+
+// {   
+//     organization : "programming hero",
+//     firstName : "jhanker",
+//     lastName : "mahabub",
+//     iaMarried : true,
+// };
+
+// user.organization = "programming hero first";
+
+// console.log(user);
+
+const user: {
+    readonly  organization : "string";//access modifier
+    firstName : string;
+    middleName? : string; // optional type
+    lastName : string;
+    isMarried : boolean;
+} =
+// { firstName : "mezba",
+//     middleName : "abedin",
+//     lastName : "forhan",
+//     isMarried : true,
+// };
+
+{   
+    organization : "programming hero",
+    firstName : "jhanker",
+    lastName : "mahabub",
+    iaMarried : true,
+};
+
+user.organization = "programming hero first";
+
+console.log(user);
